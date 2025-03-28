b952a2860af87f52a6fad48170de8772c329fe22
DavidGMont <davidgarmo@outlook.com>
2025-03-28 12:49:45 -0500

    feat(Application): Integrate file upload and serving functionality with CORS support

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,9 +1,14 @@
 import me.davidgarmo.soundseeker.product.config.DBConnection;
+import me.davidgarmo.soundseeker.product.web.servlet.FileServingServlet;
+import me.davidgarmo.soundseeker.product.web.servlet.FileUploadServlet;
 import me.davidgarmo.soundseeker.product.web.servlet.ProductServlet;
+import me.davidgarmo.soundseeker.product.web.util.CORSFilter;
 import org.apache.catalina.Context;
 import org.apache.catalina.startup.Tomcat;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.apache.tomcat.util.descriptor.web.FilterDef;
+import org.apache.tomcat.util.descriptor.web.FilterMap;
 
 import java.io.File;
 
@@ -32,10 +37,32 @@ public class Application {
             if (!docBase.exists()) {
                 docBase = new File(".");
             }
+
             Context context = tomcat.addContext("", docBase.getAbsolutePath());
+            context.setAllowCasualMultipartParsing(true);
+
+            File uploadsDir = new File(docBase, "uploads");
+            if (!uploadsDir.exists()) {
+                uploadsDir.mkdirs();
+            }
 
             Tomcat.addServlet(context, "productServlet", new ProductServlet());
             context.addServletMappingDecoded("/api/v1/products/*", "productServlet");
+
+            Tomcat.addServlet(context, "fileUploadServlet", new FileUploadServlet());
+            context.addServletMappingDecoded("/api/v1/upload/*", "fileUploadServlet");
+
+            Tomcat.addServlet(context, "fileServingServlet", new FileServingServlet());
+            context.addServletMappingDecoded("/uploads/*", "fileServingServlet");
+
+            context.addFilterDef(new FilterDef() {{
+                setFilterName("CORSFilter");
+                setFilterClass(CORSFilter.class.getName());
+            }});
+            context.addFilterMap(new FilterMap() {{
+                setFilterName("CORSFilter");
+                addURLPattern("/*");
+            }});
 
             tomcat.start();
             LOGGER.info("🚀 Tomcat server started on port 8080.");
